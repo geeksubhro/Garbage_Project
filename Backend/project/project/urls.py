ed801bcfb9a9210779231e9967001022924c78ac
subhrojeet Dutta <subhrojeet1232@gmail.com>
2023-08-12 22:49:36 +0530

    update "1.0.10"

--- Backend/project/project/urls.py
+++ Backend/project/project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from app.views import *
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ReactView.as_view(), name="Something"),
